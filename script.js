70f68fc8c253b1883eef0ffa762579df0970a8c1
yungestAmine <purpleamine@gmail.com>
2023-06-25 12:47:38 +0200

    gestiti turni e vittoria

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const board = document.querySelector('.board')
+const celle = []
 
 let player1 = 'X'
 
@@ -12,4 +13,53 @@ function start(){
         celle.push(cella)
         board.appendChild(cella)
     }
+    
+    celle.forEach(cell => {
+        cell.addEventListener('click', clickCell)
+    });
+}
+function re_start(){
+    celle.forEach(cell => {
+        cell.textContent = '';
+        cell.style.color = 'black';
+      });
+      currentPlayer = 'X';
+      gameOver = false;
+}
+function clickCell(){
+    if(endGame)
+        return
+    if(this.textContent !== '')
+        return
+    this.textContent = player1
+    this.style.color = player1 === 'X' ? 'red' : 'blue'
+
+    if(win(player1)){
+        endGame = true
+        alert(`Il giocatore ${player1} ha vinto!`)
+        re_start()
+        return
+    }
+}
+function win(p){
+    var combinazioni = 
+    [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    for (let i = 0; i < combinazioni.length; i++) {
+        const combination = combinazioni[i];
+        let hasWon = true;
+    
+        for (let j = 0; j < combination.length; j++) {
+          const index = combination[j];
+          if (celle[index].textContent !== p) {
+            hasWon = false;
+            break;
+          }
+        }   
+        if (hasWon) return true;
+      }
+    return false
 }
